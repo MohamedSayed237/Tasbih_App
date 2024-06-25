03ea47319d84a9c3b9370a8411cc095bbc51f464
MohamedSayed237 <mohamed.sayed23702@gmail.com>
2024-06-25 17:17:09 +0300

    splash screen

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:tasbih_app/screens/home_screen.dart';
+import 'package:tasbih_app/screens/splash_screen.dart';
+void main() {
+  runApp(const TasbihApp());
+}
+
+class TasbihApp extends StatelessWidget {
+  const TasbihApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: '/',
+      routes: {
+        '/':(context)=>SplashScreen(),
+        '/home':(context)=>HomeScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToHome();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(Duration(seconds: 2), () {});
+    Navigator.pushReplacementNamed(context, '/home');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color(0xc8e1e5),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/icon.jpg'),
+              SizedBox(
+                height: 20,
+              ),
+              CircularProgressIndicator(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
